a5612b39bde0710f30712a2912c11f40b49b7d0d
ehfhcrc <ehenrich@fredhutch.org>
2017-03-23 14:23:09 -0700

    added eset as output option

--- R/getCombinedData.R
+++ R/getCombinedData.R
@@ -30,28 +30,77 @@
   return(em)
 }
 
+.match_subs <- function(em, cdata){
+  gs <- em$gene_symbol
+  tmp <- em[ , which(colnames(em) %in% unique(cdata$participant_id)), with = F ]
+  tmp$gene_symbol <- gs
+  return(tmp)
+}
+
+# The method expects a list (elem) with two dataframes representing EM and cData
+.prep_eset <- function(elem){
+  fData <- data.frame(elem$EM$gene_symbol, stringsAsFactors = F)
+  rownames(fData) <- elem$EM$gene_symbol
+  colnames(fData) <- "gene_symbol"
+  expr_em <- elem$EM[ , -c("gene_symbol") ]
+  
+  pData <- elem$cData[ order(match(elem$cData$participant_id, colnames(expr_em))), ]
+  rownames(pData) <- pData$participant_id
+  pData <- droplevels(pData)
+  
+  eset <- ExpressionSet(assayData = as.matrix(expr_em),
+                        featureData = AnnotatedDataFrame(fData),
+                        phenoData = AnnotatedDataFrame(pData))
+}
+
 # Main Method --------------------------------------------------------
+# NOTE: clin_sum controls whether the clinical data is summarized if there is
+# more than one row per subject and, if so, how.
+# clin_sum options include --
+# 0: not summarized, 1: summarized by max value, 2: summarized by average value
+# summarizing by average will cause the data to drop the virus column if HAI
 
 #' @importFrom hash hash
+#' @importFrom dplyr filter mutate distinct ungroup
+#' @importFrom magrittr %>%
+#' @importFrom Biobase ExpressionSet
 .ISCon$methods(
-  getCombinedData = function(clin_data, 
+  getCombinedData = function(clin_data = "hai", 
                              clin_time = NULL,
+                             clin_sum = 0,
                              EM_list = NULL,
                              ge_time = 0,
                              genes = NULL, 
-                             output = "all"
+                             merge_by = "all",
+                             eset = FALSE
                              ){
     
-    # make sure clinical data is appropriate
-    cdata_allowed <- c("hai")
+  
+    # make sure arguments are allowed and handle unwanted vals gracefully
+    cdata_allowed <- c("hai","neut_ab_titer")
     if( !(clin_data %in% cdata_allowed) ){ 
-      stop(paste0("clinical data type must be one of the following: ", cdata_allowed))
+      stop(paste0("clinical data input must be one of the following: ", cdata_allowed))
     }
     
-    output_allowed <- c("all", "study", "em")
-    if( !(output %in% output_allowed) ){ 
-      stop(paste0("merge must be one of the following: ", output_allowed))
+    csum_allowed <- c(0, 1, 2)
+    if( !(clin_sum %in% csum_allowed) ){ 
+      stop(paste0("cdata summary input must be one of the following: ", csum_allowed))
     }
+    
+    merge_allowed <- c("all", "study", "em")
+    if( !(merge_by %in% merge_allowed) ){ 
+      stop(paste0("merge type must be one of the following: ", merge_allowed))
+    }
+    
+    if(eset == T & clin_sum == 0){
+      stop("clinical data must be summarized to output an eset, please use 'clin_sum' = 1 or 2")
+    }
+    
+    if(eset == T & is.null(clin_time)){
+      stop("clinical data must be summarized to output an eset, 
+           please set 'clin_time' to value in study_time_collected")
+    }
+    
     
     # Get all GE data into list of dfs and combine
     if( is.null(EM_list) ){ EM_list <- GeneExpressionMatrices()$name }
@@ -80,13 +129,33 @@
     filt_ems <- lapply(clean_ems, .filterGenes, genes = common_genes)
     
     
-    # pull clinical data with filter for subs and filter for values col
+    # pull clinical data with filter for subs and clean
     sub_filter <- makeFilter(c("participant_id", 
                                "IN", 
                                paste0(unique(pheno$`Participant ID`), collapse = ";")))
     
     cdata <- getDataset(clin_data, colFilter = sub_filter)
     if(!is.null(clin_time)){ cdata <- cdata[ which(cdata$study_time_collected == clin_time), ] }
+    keep <- which(colnames(cdata) != "virus")
+    if( clin_sum != 0){
+      cdata <- cdata[ , keep, with = F] # avoids use of dplyr::select()
+      if( clin_sum == 1 ){
+        cdata <- cdata %>%
+          group_by(participant_id) %>%
+          filter(value_reported == max(value_reported)) %>%
+          distinct(.keep_all = TRUE) %>%
+          ungroup()
+      } else if( clin_sum == 2){
+        cdata <- cdata %>%
+          group_by(participant_id) %>%
+          mutate(value_reported = mean(value_reported)) %>%
+          distinct(.keep_all = TRUE) %>%
+          ungroup()
+      }
+    }
+    
+    # make sure only cData subs are in EM
+    filt_ems <- lapply(clean_ems, .match_subs, cdata)
     
     # for mapping EM names to study ID
     em_runs <- data.table(labkey.selectRows(
@@ -97,15 +166,16 @@
     
     # return output in either one merged EM, by study, or by EM name
     res <- list()
-    if(output == "all"){
+    if(merge_by == "all"){
       res$EM <- Reduce(merge, filt_ems)
       res$cData <- cdata
       
       message("----Data for single EM and cData set---")
       message(paste0("Common Genes: ", length(common_genes)))
       message(paste0("Number of Subjects: ", length(colnames(res$EM)) - 1))
+      if(eset == TRUE){ res <- .prep_eset(res) }
       
-    }else if(output == "study"){
+    }else if(merge_by == "study"){
       studies <- unique(gsub(".*\\.", "", cdata$participant_id))
       res <- sapply(studies, simplify = F, USE.NAMES = T, FUN = function(sdy){
         tmp <- list()
@@ -121,6 +191,7 @@
         message(paste0("Number of Genes: ", dim(tmp$EM)[1]))
         return(tmp)
       })
+      if(eset == TRUE){ res <- lapply(res, .prep_eset) }
       
     }else{
       em_nms <- em_runs$Name[ which(em_runs$Name %in% gsub("_sum","", names(filt_ems))) ]
@@ -136,9 +207,14 @@
         
         return(tmp)
       })
+      if(eset == TRUE){ res <- lapply(res, .prep_eset) }
     }
     
     return(res)
   }
+)
+
+.ISCon$methods(
+  
 )
 
